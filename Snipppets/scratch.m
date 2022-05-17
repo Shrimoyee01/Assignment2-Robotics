a425bb0b98b1ba83cd500fc49e12c46f2a805a5f
Botherdwalrus <lydia.angus@gmail.com>
2022-05-17 15:58:57 +1000

    blender files

--- Snipppets/scratch.m
+++ Snipppets/scratch.m
@@ -2,7 +2,7 @@
 clc
 clf
 
-Test;
+% Test;
 % camlight
 
 % robot = IRB120test();
@@ -11,9 +11,9 @@ Test;
 % robot.advancedTeach;
 % robot.move(startLocation, endLocation, isHolding);
 
-% location1 = transl(1,0,0);
+location1 = transl(1,0,0);
 
-% cup1 = Thing("cup4",location1);
+cup1 = Thing("person5",location1);
 
 % disp("press enter to continue");
 % pause;
