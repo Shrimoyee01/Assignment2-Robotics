e4716e5c3da1e96e41623df4161b2937d4973d22
Botherdwalrus <lydia.angus@gmail.com>
2022-05-17 17:49:34 +1000

    adding order buttons and switch case

--- Snipppets/@Test/getStartingPositions.m
+++ Snipppets/@Test/getStartingPositions.m
@@ -8,21 +8,24 @@ function [cupStartLocations, cupEndLocations, robotBase ...
 
     %tableHeight is used for the robot base, cup start and end locations 
 
-    tableH = 0.4;
-    cupW = 0.2669;
-    cupH = 0.0667;
+%     tableH = 0.4;
+%     cupW = 0.2669;
+%     cupH = 0.0667;
 
     %Cup start locations
-    cupStartLocations{1} = transl(0.5, -0.5, 0);
-    cupStartLocations{2} = transl(0.2, -0.5, 0);
-    cupStartLocations{3} = transl(0.4, -0.5, 0);
+    cupStartLocations{1} = transl(1.2, -1.05,  1.2);
+    cupStartLocations{2} = transl(1,   -1.05,  1.2);
+    cupStartLocations{3} = transl(0.8, -1.05,  1.2);
 
     %Robot Base Location
-    robotBase = [0, 0, 0];
+    robotBase = [1, 0.2, 1.0];
 
-    cupEndLocations{1}= transl( robotBase(1,1) ,0.5, 0.5  );
-    cupEndLocations{2}= transl( robotBase(1,1) ,0.5, 0  );
-    cupEndLocations{3}= transl( robotBase(1,1) ,0.5, 0  );
+%     cupEndLocations{1}= transl( robotBase(1,1) ,0.5, 0.5  );
+%     cupEndLocations{2}= transl( robotBase(1,1) ,0.5, 0  );
+%     cupEndLocations{3}= transl( robotBase(1,1) ,0.5, 0  );
+    cupEndLocations{1}= transl( 0.5 ,0, 1.7  );
+    cupEndLocations{2}= transl( 0.5 ,0, 1.5  );
+    cupEndLocations{3}= transl( 0.5 ,0, 1.5  );
 
 
 end
